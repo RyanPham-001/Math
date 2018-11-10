d35afe219dce9cc13466cd72d30505e890bed114
luckiuze <44855019+luckiuze@users.noreply.github.com>
2018-11-10 10:12:40 -0500

    Create Operations.py

---
+++ Operations.py
@@ -0,0 +1,8 @@
+print("What operation would you like to do?")
+operation = input("1.) Addition\n2.)Subtraction\n3.)Multiplication\n4.)Division")
+if(operation== "1"):
+  number()
+
+def number ():
+#add lines for user to add as many as they want
+  number1 = 
