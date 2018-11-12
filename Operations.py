42a9a7817f2a258a143d576577c4ea18f02544c3
RyanPham-001 <44855019+RyanPham-001@users.noreply.github.com>
2018-11-12 17:23:29 -0500

    Update Operations.py

--- Operations.py
+++ Operations.py
@@ -1,7 +1,11 @@
 print("What operation would you like to do?")
 operation = input("1.) Addition\n2.)Subtraction\n3.)Multiplication\n4.)Division")
 if(operation== "1"):
-  number()
+  first_number = input("What is your first number? ")
+  second_number = input("What is your second number? ")
+  #takes numbers from user
+  print (first_number+ " + " + second_number + " = " + (float(first_number)+float(second_number))
+  #adds the first number from second number
 elif(operation == "2"):
   first_number = input("What is your first number? ")
   second_number = input("What is your second number? ")
@@ -12,7 +16,7 @@ elif(operation == "3"):
   first_number = input("What is your first number? ")
   second_number = input("What is your second number? ")
   #take numbers from user in order of their ask
-  print(first_number + " * " + second_number + " = " + (float(first_number)-float(second_number))
+  print(first_number + " * " + second_number + " = " + (float(first_number)*float(second_number))
   #multiplies the first number by the second number. 
   # redo addition 
 elif(operation == "4"):
