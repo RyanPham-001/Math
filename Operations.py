9c7c7ba0e8fb54f4b05202d845cbd297a5741fae
RyanPham-001 <44855019+RyanPham-001@users.noreply.github.com>
2018-11-12 17:22:36 -0500

    Update Operations.py

--- Operations.py
+++ Operations.py
@@ -6,7 +6,7 @@ elif(operation == "2"):
   first_number = input("What is your first number? ")
   second_number = input("What is your second number? ")
   #takes numbers from user
-  print (first_number+ " - " + second_number + " = " + (int(first_number)-int(second_number))
+  print (first_number+ " - " + second_number + " = " + (float(first_number)-float(second_number))
   #subtracts the first number from second number
 elif(operation == "3"):
   first_number = input("What is your first number? ")
